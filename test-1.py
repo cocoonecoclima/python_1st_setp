d226c4609ef495addbdb5e1d3f27eb1086e543d9
Cocoon Ecoclima <support@cocoon.gr>
2021-11-25 12:39:38 +0200

    new add

--- test-1.py
+++ test-1.py
@@ -3,8 +3,8 @@ from openpyxl import Workbook, workbook
 workbook = Workbook()
 sheet = workbook.active
 
-sheet["A1"] = "HELLO"
-sheet["B1"] = "THERE"
+sheet["A1"] = input("Give me 1st input: ")
+sheet["B1"] = input("Give me second ipnut: ")
 
 workbook.save(filename="1st_excel_frompy.xlsx")
 
